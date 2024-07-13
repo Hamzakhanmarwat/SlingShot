1f0b302c9b15bc6833d0373c631e68ce1c713719
Hamza Davis <123638084+Hamzakhanmarwat@users.noreply.github.com>
2024-07-13 13:08:20 +0500

    Camera Panning and Physics
    Added Camera Panning and Ground Physics to make things bouncy

---
+++ Assets/Scripts/PanZoom.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanZoom : MonoBehaviour
+{
+    Vector3 touchStart;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            touchStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            Vector3 direction = touchStart - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Camera.main.transform.position += direction;
+        }
+    }
+}
