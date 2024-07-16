b1c96d9a04437d42f14e5da7d5f109cd94a58152
syedmdev <smuhammadabdullah002@gmail.com>
2024-07-16 18:55:43 +0500

    8 platforms for bouncing

---
+++ Assets/Scripts/Bounce Scripts/Bounce Down.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceDown : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D player;
+    public float speedChange;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        player.velocity += new Vector2(0, speedChange * -1f);
+    }
+}
