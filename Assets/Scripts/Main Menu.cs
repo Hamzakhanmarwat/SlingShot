b6e554a6a4c70a72b39b0356f4bf9f1900b46cf3
AbdullahSafdarW <abdullahsa.pisjesy91@gmail.com>
2024-07-23 10:19:01 +0500

    Add files via upload

---
+++ Assets/Scripts/Main Menu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+   
+    // Update is called once per frame
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync("Sling Scene");
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadSceneAsync("Main Menu");
+    }
+}
